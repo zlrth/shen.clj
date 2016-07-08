a400005f37ff1434284fd065a86c77fef5127b32
Matthew Marks <matthewfmarks@gmail.com>
2016-07-08 09:03:52 -0400

    setting "target" fixes build. now have uberjar for emacs interop

--- project.clj
+++ project.clj
@@ -2,7 +2,7 @@
   :description "Shen is a portable functional programming language by Mark Tarver"
   :license {:name "Shen License"
             :url "http://www.shenlanguage.org/license.html"}
-  :url "https://github.com/hraberg/shen.clj"
+  :url "https://github.com/zlrth/shen.clj"
   :repositories {"sonatype snapshots"
                  "https://oss.sonatype.org/content/repositories/snapshots/"}
   :dependencies [[org.clojure/clojure "1.8.0"]]
@@ -10,7 +10,7 @@
   :plugins [[lein-swank "1.4.5"]
             [lein-difftest "2.0.0"]]
   :compile-path "classes"
-  :target-path ""
+  :target-path "target"
   :aot [shen.primitives shen.install shen]
   :main shen.install
   :min-lein-version "2.0.0")
